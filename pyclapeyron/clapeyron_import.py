9fd117e4aef47f172da36f8a0515ccb9f9c8ac8b
se-schmitt <se.s.schmitt@gmail.com>
2025-09-26 19:39:04 +0200

    Better Python wrapper

--- pyclapeyron/clapeyron_import.py
+++ pyclapeyron/clapeyron_import.py
@@ -1,9 +1,39 @@
-from juliacall import Main as jl
+import numpy as np
+import juliacall
+jl = juliacall.Main
 
 jl.seval("using Clapeyron")
 cl_exports = [str(n) for n in jl.names(jl.Clapeyron) if not str(n).startswith('@')]
-cl_exports = [s.replace('!','_b') for s in cl_exports]
-for s in cl_exports:
-    exec(f'{s} = jl.{s}')
 
-__all__ = [s for s in cl_exports]
+def create_wrapper(julia_func):
+
+    def wrapper(*args, **kwargs):
+        result_from_julia = julia_func(*args, **kwargs)
+
+        if isinstance(result_from_julia, tuple):
+            processed_results = []
+            for item in result_from_julia:
+                if isinstance(item, juliacall.ArrayValue):
+                    processed_results.append(np.asarray(item))
+                else:
+                    processed_results.append(item)
+            return tuple(processed_results)
+        elif isinstance(result_from_julia, juliacall.ArrayValue):
+            return np.asarray(result_from_julia)
+        else:
+            return result_from_julia
+
+    # Set name and docs
+    wrapper.__name__ = julia_func.__name__.replace('!','_b')
+    wrapper.__doc__ = f"Python wrapper for Julia's `{wrapper.__name__}` function."
+    
+    return wrapper
+
+module_globals = globals()
+
+for func_name in cl_exports:
+    func = getattr(jl.Clapeyron, func_name)
+    python_wrapper = create_wrapper(func)
+    
+    # Add to module globals
+    module_globals[python_wrapper.__name__] = python_wrapper
